f1bf6000ab8e7935dc5fc9e3ab8758c49fb76f7b
Tarek Abdo <tarekk.abdo22@gmail.com>
2025-01-09 22:34:20 +0200

    add more validation

--- components/ManageExpenses/ExpenseForm.js
+++ components/ManageExpenses/ExpenseForm.js
@@ -6,20 +6,26 @@ import { get } from "react-native/Libraries/TurboModule/TurboModuleRegistry";
 import { getFormattedDate } from "../../util/data";
 
 const ExpenseForm = ({ isEditing, onCancel, onSubmit, defaultValues }) => {
-  console.log(defaultValues.date.toISOString().slice(0, 10));
-  console.log(getFormattedDate(defaultValues.date));
-
-  const [inputValues, setInputValues] = useState({
-    amount: defaultValues ? defaultValues.amount.toString() : "",
-    date: defaultValues ? getFormattedDate(defaultValues.date) : "",
-    description: defaultValues ? defaultValues.description : "",
+  const [inputs, setInputs] = useState({
+    amount: {
+      value: defaultValues ? defaultValues.amount.toString() : "",
+      isValid: !!defaultValues,
+    },
+    date: {
+      value: defaultValues ? getFormattedDate(defaultValues.date) : "",
+      isValid: !!defaultValues,
+    },
+    description: {
+      value: defaultValues ? defaultValues.description : "",
+      isValid: !!defaultValues,
+    },
   }); // the value on any input will be always string alwaysssssss
 
   function inputChangeHandler(inputIdentifier, enteredAmount) {
-    setInputValues((currInputValues) => {
+    setInputs((curInputs) => {
       return {
-        ...currInputValues,
-        [inputIdentifier]: enteredAmount,
+        ...curInputs,
+        [inputIdentifier]: { value: enteredAmount, isValid: true },
       };
     });
   }
@@ -28,9 +34,9 @@ const ExpenseForm = ({ isEditing, onCancel, onSubmit, defaultValues }) => {
     // submit the form
     console.log("Submitting form", inputValues);
     const expenseData = {
-      amount: +inputValues.amount,
-      date: new Date(inputValues.date),
-      description: inputValues.description,
+      amount: +inputs.amount.value,
+      date: new Date(inputs.date.value),
+      description: inputs.description.value,
     };
 
     const amountIsValid = !isNaN(expenseData.amount) && expenseData.amount > 0;
@@ -38,11 +44,25 @@ const ExpenseForm = ({ isEditing, onCancel, onSubmit, defaultValues }) => {
     const descriptionIsValid = expenseData.description.trim().length > 0;
 
     if (!amountIsValid || !dateIsValid || !descriptionIsValid) {
-      return;
+      setInputs((curInput) => {
+        return {
+          amount: { value: curInput.amount.value, isValid: amountIsValid },
+          date: { value: curInput.date.value, isValid: dateIsValid },
+          description: {
+            value: curInput.description.value,
+            isValid: descriptionIsValid,
+          },
+        };
+      });
     }
 
     onSubmit(expenseData);
   }
+
+  const formIsValid =
+    !inputs.amount.isValid ||
+    !inputs.date.isValid ||
+    !inputs.description.isValid;
 
   return (
     <View style={styles.form}>
@@ -54,7 +74,7 @@ const ExpenseForm = ({ isEditing, onCancel, onSubmit, defaultValues }) => {
           textInputConfig={{
             keyboardType: "decimal-pad",
             onChangeText: inputChangeHandler.bind(this, "amount"),
-            value: inputValues.amount,
+            value: inputs.amount.value,
           }}
         />
         <Input
@@ -64,7 +84,7 @@ const ExpenseForm = ({ isEditing, onCancel, onSubmit, defaultValues }) => {
             placeholder: "YYYY-MM-DD",
             maxLenght: 10,
             onChangeText: inputChangeHandler.bind(this, "date"),
-            value: inputValues.date,
+            value: inputs.date.value,
           }}
         />
       </View>
@@ -73,9 +93,12 @@ const ExpenseForm = ({ isEditing, onCancel, onSubmit, defaultValues }) => {
         textInputConfig={{
           multiline: true,
           onChangeText: inputChangeHandler.bind(this, "description"),
-          value: inputValues.description,
+          value: inputs.description.value,
         }}
       />
+      {formIsValid && (
+        <Text>Invalid Inputs - please check your entered data!</Text>
+      )}
       <View style={styles.buttons}>
         <Button mode="flat" onPress={onCancel} style={styles.button}>
           Cancel
